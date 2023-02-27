05ffec57f063bcb1d47f729214dc8ba50a1e7c89
lekhoi2709 <ledinhkhoimd@gmail.com>
2023-02-27 08:49:11 +0700

    add navigation drawer

--- App.jsx
+++ App.jsx
@@ -1,11 +1,25 @@
-
 import AppContainer from "./src/components/app-container";
 import Main from "./src/screens/main";
+import { Box } from "native-base";
+import { NavigationContainer } from "@react-navigation/native";
+import { createDrawerNavigator, DrawerContentScrollView } from "@react-navigation/drawer";
+
+const Drawer = createDrawerNavigator();
+
+function MyNav() {
+   return (
+      <Box flex={1}>
+         <Drawer.Navigator>
+            <Drawer.Screen name="Main" component={Main} />
+         </Drawer.Navigator>
+      </Box>
+   )
+}
 
 export default function App() {
    return (
       <AppContainer>
-         <Main />
+         <MyNav />
       </AppContainer>
    );
 }
