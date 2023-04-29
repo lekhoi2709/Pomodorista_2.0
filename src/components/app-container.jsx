bdb9b2c1b09468acedc2af80cdfece22a1b72af3
lekhoi2709 <ledinhkhoimd@gmail.com>
2023-04-29 12:50:30 +0700

    adjust theme

--- src/components/app-container.jsx
+++ src/components/app-container.jsx
@@ -1,10 +1,11 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { NativeBaseProvider } from "native-base";
+import theme from '../theme.js'
 
 export default function AppContainer(props) {
    return (
       <NavigationContainer className="w-screen h-screen">
-         <NativeBaseProvider className="w-full h-full">
+         <NativeBaseProvider className="w-full h-full" theme={theme}>
             {props.children}
          </NativeBaseProvider>
       </NavigationContainer>
